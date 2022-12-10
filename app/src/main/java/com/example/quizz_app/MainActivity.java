fb03fa2b85ac0beda8b0f9c24ecb8d32de04ae5d
TranHoang2505g <huyhoang25052002@gmail.com>
2022-12-10 16:06:26 +0700

    Build MainActivity

--- app/src/main/java/com/example/quizz_app/MainActivity.java
+++ app/src/main/java/com/example/quizz_app/MainActivity.java
@@ -1,14 +1,31 @@
-package com.example.quizz_app;
+package com.example.quizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.quizapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btnStart.setOnClickListener(view -> {
+            if(binding.etUsername.getText().toString().isEmpty()) {
+                Toast.makeText(this, "Please enter your name", Toast.LENGTH_SHORT).show();
+            } else {
+                Intent intent = new Intent(this, HomeActivity.class );
+                intent.putExtra(Constants.USER_NAME, binding.etUsername.getText().toString().trim());
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
